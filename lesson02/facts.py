aa2c7dcf6a4d2ce4155481ffd13e0f238e3a3dee
Liza <lizazhemchuzhina@mail.ru>
2019-11-15 04:57:48 -0500

    fatcts

--- lesson02/facts.py
+++ lesson02/facts.py
@@ -5,8 +5,8 @@ import sys
 
 Person = collections.namedtuple('Person', ['github_login', 'first_name', 'last_name', 'facts'])
 people = [
-    Person(github_login='example', first_name='Name', last_name='Surname',
-           facts=['Fact-right.', 'Fact-write.', 'Fact-right.']),
+    Person(github_login='lizazhemchuzhina', first_name='Liza', last_name='Zhemchuzhina',
+           facts=['from Russia with love.', 'liza dura', 'ternarniki is the best']),
 ]
 
 answers = {}
